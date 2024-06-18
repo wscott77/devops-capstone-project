c2f2520e8e06e974ea2b45931e42d79c676bd279
wscott77 <greatsctt7@gmail.com>
2024-06-18 12:16:25 -0400

    Adding delete customer.

--- service/routes.py
+++ service/routes.py
@@ -106,8 +106,17 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+        Delete an Account
+        Delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -164,3 +164,9 @@ class TestAccountService(TestCase):
         new_account.name = "Updated Name"
         resp = self.client.put(f"{BASE_URL}/0", json=new_account.serialize())
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND) 
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
