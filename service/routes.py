8797e093ebb8cbdf78a486706ef049575971c2ab
wscott77 <greatsctt7@gmail.com>
2024-06-19 08:11:33 -0400

    completed ci build 3 plus code clean up

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -70,10 +71,8 @@ def list_accounts():
     accounts = Account.all()
 
     account_list = [account.serialize() for account in accounts]
-
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
-
 
 
 ######################################################################
@@ -86,13 +85,14 @@ def get_accounts(account_id):
         Read an Account based the account_id that is requested
     """
     app.logger.info("Read Customer Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
 
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -108,7 +108,7 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        
+
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
